21c601a6f1e5c440598ccfac48a2d0b6d77b883e
adrian83 <adrianbrzoza1983@gmail.com>
2020-02-10 13:06:28 +0100

    [fe] moving activities between stages

--- robome-fe/src/components/tiles/LinkWithIcon.js
+++ robome-fe/src/components/tiles/LinkWithIcon.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+var margin = {marginLeft: '10px', marginRight: '10px'}
+
 class LinkWithIcon extends Component {
 
     render() {
+        const to = this.props.to ? this.props.to : "";
+        const text = this.props.text ? (<span style={margin} >{this.props.text}</span>) : "";
+
         return (
-            <span style={{marginLeft: '10px', marginRight: '10px'}} >
-                <Link to={this.props.to} 
-                        onClick={this.props.onClick}>
-                    <i class={this.props.icon}>{this.props.text}</i>
+            <span style={margin} >
+                <Link 
+                    to={to} 
+                    onClick={this.props.onClick}>
+                    <i className={this.props.icon}>{text}</i>
                 </Link>
             </span>);
     }
